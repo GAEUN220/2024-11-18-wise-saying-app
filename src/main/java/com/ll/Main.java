ec986a9f6793b77281ee2d5cb6e3261237fe98ff
gaeun220 <gaeun2701@gmail.com>
2024-11-21 16:46:55 +0900

    5단계

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,5 +1,7 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -10,6 +12,8 @@ public class Main {
 }
 
 class App {
+    List<wiseSaying> wiseSayings = new ArrayList<>(); // 리스트 생성
+
     public void run() {
         System.out.println("== 명언 앱 ==");
 
@@ -18,6 +22,7 @@ class App {
         int lastId = 0;
 
         while (true) {
+
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
 
@@ -29,10 +34,39 @@ class App {
                 System.out.print("작가 : ");
                 String author = scanner.nextLine();
                 int id = ++lastId;
-                System.out.println("%d번 명언이 등록되었습니다.".formatted(id));
+
+                wiseSayings.add(new wiseSaying(id, content, author)); // 리스트 추가
+
+                System.out.println("%d번 명언이 등록되었습니다.".formatted(id)); // 출력
+            } else if (cmd.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+
+                int i = wiseSayings.size() - 1;
+                while (i >= 0) {
+                    System.out.println(wiseSayings.get(i));
+                    i--;
+                }
             }
         }
 
         scanner.close();
+    }
+}
+
+class wiseSaying {
+    int id;
+    String content;
+    String author;
+
+    wiseSaying(int id, String content, String author) {
+        this.id =id;
+        this.content = content;
+        this.author = author;
+    }
+
+    @Override
+    public String toString() { //여기는 public 붙여야함
+        return id + " / " + author + " / " + content;
     }
 }
