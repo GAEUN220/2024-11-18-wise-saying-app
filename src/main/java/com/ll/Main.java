9eef088f5eaa3c03ee73923245dec07d88538bba
gaeun220 <gaeun2701@gmail.com>
2024-11-25 22:30:47 +0900

    level 2

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -21,8 +21,15 @@ class App {
             System.out.print("명령) ");
 
             String cmd = scanner.nextLine();
+
             if (cmd.equals("종료")) {
                 break;
+            } else if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                String content = scanner.nextLine();
+
+                System.out.print("작가 : ");
+                String author = scanner.nextLine();
             }
 
         }
